e5cb588f61014a3ff2ebff70ee3d506e54d066fa
mohammadhasann <mohamadhasanf71@gmail.com>
2025-04-10 22:51:04 +0330

    EX3_LM_1_2_A

--- Ex/Ex3/LM_1_2.java
+++ Ex/Ex3/LM_1_2.java
@@ -1,55 +1,97 @@
 package Ex.Ex3;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.ArrayList;
 
+class Booki {
+    private String bookName;
+    private String author;
+    private int pageCount;
+    private int publicationYear;
+    public Booki(String bookName, String author, int pageCount, int publicationYear) {
+        this.bookName = bookName;
+        this.author = author;
+        this.pageCount = pageCount;
+        this.publicationYear = publicationYear;
+    }
+    public String getBookName() {
+        return bookName;
+    }
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public int getPageCount() {
+        return pageCount;
+    }
+    public void setPageCount(int pageCount) {
+        this.pageCount = pageCount;
+    }
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+}
+class Studenti {
+    private String firstName;
+    private String lastName;
+    private String studentId;
+    private String major;
+    public Studenti(String firstName, String lastName, String studentId, String major) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.studentId = studentId;
+        this.major = major;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public  String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getStudentId() {
+        return studentId;
+    }
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+    public String getMajor() {
+        return major;
+    }
+    public void setMajor(String major) {
+        this.major = major;
+    }
+}
 public class LM_1_2 {
-    public static void saveBooks(Book[] books) {
+    public static void saveBooks(ArrayList<Booki> books) {
         try (FileWriter writer = new FileWriter("books.txt")) {
-            for (Book book : books) {
-                writer.write(book.getBookName() + "-" + book.getAuthor() + "-" + book.getPageCount() + "-" + book.getPublicationYear() + ".");
+            for (Booki booki : books) {
+                writer.write(booki.getBookName() + "-" + booki.getAuthor() + "-" + booki.getPageCount() + "-" + booki.getPublicationYear() + ".");
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
-    public static void saveStudents(Student[] students) {
+    public static void saveStudents(ArrayList<Studenti> students) {
         try (FileWriter writer = new FileWriter("students.txt")) {
-            for (Student student : students) {
-                writer.write(student.getFirstName() + "-" + student.getLastName() + "-" + student.getStudentId() + "-" + student.getMajor() + ".");
+            for (Studenti studenti : students) {
+                writer.write(studenti.getFirstName() + "-" + studenti.getLastName() + "-" + studenti.getStudentId() + "-" + studenti.getMajor() + ".");
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    public static Book[] readBooks() {
-        Book[] books = new Book[4];
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("books.txt"))) {
-            String line;
-            int index = 0;
-            while ((line = bufferedReader.readLine()) != null && index < books.length) {
-                String[] data = line.split(",");
-                books[index++] = new Book(data[0], data[1], Integer.parseInt(data[2]), Integer.parseInt(data[3]));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    public static Student[] readStudents() {
-        Student[] students = new Student[3];
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("stuents.txt"))) {
-            String line;
-            int index = 0;
-            while ((line = bufferedReader.readLine()) != null && index < students.length) {
-                String[] data = line.split(",");
-                students[index++] = new Student(data[0], data[1], data[2], data[3]);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return students;
     }
 }
